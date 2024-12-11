e93ec1bef8f70bfab49ece211449f634a50a9823
baronvonperko <chris@chrisperko.net>
2024-12-10 22:21:38 -0500

    Step 09

--- src/app/data/item-data.ts
+++ src/app/data/item-data.ts
@@ -1,4 +1,5 @@
 import {Item} from '../models/item';
+import {Injectable} from '@angular/core';
 
 export const serverItems: Item[] = [
   { id: 1, name: 'Furby', price: 34.99, image: 'furby.png' },
@@ -6,3 +7,14 @@ export const serverItems: Item[] = [
   { id: 3, name: 'Talkboy', price: 29.99, image: 'talkboy.jpg' },
   { id: 4, name: 'Crossfire', price: 19.99, image: 'crossfire.jpg' },
 ];
+
+@Injectable({ providedIn: 'root' })
+export class ApiService {
+  load(): Promise<Item[]> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(serverItems);
+      }, 2000);
+    });
+  }
+}
--- src/app/state/store.ts
+++ src/app/state/store.ts
@@ -1,7 +1,7 @@
 import {StoreItem} from '../models/item';
 import {patchState, signalStore, withComputed, withHooks, withMethods, withState} from '@ngrx/signals';
-import {serverItems} from '../data/item-data';
-import {computed} from '@angular/core';
+import {ApiService, serverItems} from '../data/item-data';
+import {computed, inject} from '@angular/core';
 import {setEntities, updateEntities, updateEntity, withEntities} from '@ngrx/signals/entities';
 
 type State = {
@@ -23,20 +23,22 @@ export const AppStore = signalStore(
     totalItemsInCart: computed(() => store.itemsInCart().length),
     totalPrice: computed(() => store.itemsInCart().reduce((acc, curr) => acc + curr.price, 0)),
   })),
-  withMethods((store) => ({
+  withMethods((store, api = inject(ApiService)) => ({
     toggleInCart(itemToUpdate: StoreItem, addToCart: boolean) {
       patchState(
         store,
         updateEntity({id: itemToUpdate.id, changes: {inCart: addToCart} }),
       )
+    },
+    async loadData() {
+      await api.load()
+        .then(items => patchState(store, setEntities(items), {status: 'success'}))
+        .catch(() => patchState(store, {status: 'error'}));
     }
   })),
   withHooks({
     onInit(store) {
-      patchState(
-        store,
-        setEntities(serverItems)
-      )
+      store.loadData();
     }
   })
 )
